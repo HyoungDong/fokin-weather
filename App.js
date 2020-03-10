ce68ecfe9df7bcd6ca6e099ba5f45fc292df6e33
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-11 01:43:27 +0900

    Example of style

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Loading from "./Loading"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style ={styles.yellowView}>
+      <Text>Hello1</Text>
+      </View>
+      <View style ={styles.blueView}>
+      <Text>Hello2</Text>
+      </View>
+    </View>
+  );
+}
+
+
