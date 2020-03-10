82d897ee50716c482c8feca080e7c52d4b7dc46a
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-11 01:51:31 +0900

    Loading screen

--- Loading.js
+++ Loading.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading(){
     return <View style ={styles.container}>
-        <Text>Getting the fucking weather</Text>
+        <Text style ={styles.text}>Getting the fucking weather</Text>
     </View>
 }
 const styles = StyleSheet.create({
     container:{
-        flex :1
+        flex :1,
+        justifyContent:"flex-end",
+        paddingHorizontal:30,
+        paddingVertical:100,
+        backgroundColor:"#FDF6AA",
+
+    },
+    text: {
+        color:"#2c2c2c",
+        fontSize:30
     }
 });
