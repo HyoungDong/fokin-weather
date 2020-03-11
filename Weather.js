5ed1b8c522443f114b9e7dfbaf3a19bad0c83e9d
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-11 18:07:35 +0900

    Display Icons which come from expo/vector-icons

--- Weather.js
+++ Weather.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text, StyleSheet } from "react-native";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function Weather({temp}){
     return (
     <View style ={styles.container}>
-        <Text>{temp}</Text>
+        <View style = {styles.Uphalf}>
+        <MaterialCommunityIcons size = {96} name = "weather-lightning-rainy"/>
+        <Text style = {styles.temp}>{temp}•</Text>
+        </View>
+        <View style = {styles.Downhalf}>
+        </View>
     </View>
     );
 }
 
 Weather.propTypes = {
-    temp:PropTypes.number.isRequired
+    temp:PropTypes.number.isRequired,
+    condition:PropTypes.oneOf(["Thunderstorm","Drizzle","Rain","Snow","Atmosphere","Clear","Clouds","Haze","Mist","Dust"]).isRequired
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +25,16 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:"center",
         alignItems:"center" 
+    },
+    Uphalf:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    Downhalf:{
+        flex:1
+    },
+    temp:{
+        fontSize:42
     }
 });
