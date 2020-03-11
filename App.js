1b57ff0d9c42a8f088cbb210560923b22cb0a59a
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-11 15:44:32 +0900

    Request Permission to users and get their location informations

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import React from "react";
 import Loading from "./Loading"
+import { Alert } from "react-native";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style ={styles.yellowView}>
-      <Text>Hello1</Text>
-      </View>
-      <View style ={styles.blueView}>
-      <Text>Hello2</Text>
-      </View>
-    </View>
-  );
+const API_KEY = "f4453a6f7504a6fe09837faa1f1ac0c7"; 
+
+export default class extends React.Component{
+  state ={
+    isLoading:true,
+
+  };
+  getlocation = async() =>{
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords:{ latitude, longitude} } = await Location.getCurrentPositionAsync();
+      this.setState({isLoading:false});
+      //Send to API and get weather
+      //console.log(coords.latitude, coords.longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+
+  }
+  componentDidMount(){
+    this.getlocation();
+  }
+  render(){
+    const { isLoading } = this.state;
+    return isLoading?<Loading/>:null;
+  }
 }
 
 
