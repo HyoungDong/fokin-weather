71c5f2e57a0e34c529c7da52d55f191471c4d99a
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-11 18:53:57 +0900

    Add all condition and following icons, background colors

--- Weather.js
+++ Weather.js
@@ -1,17 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet,StatusBar } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-export default function Weather({temp}){
+import { LinearGradient } from "expo-linear-gradient";
+
+const weatherOptions = {
+    Haze:{
+        iconName:"weather-hail",
+        gradient: ["#304352","#d7d2cc"]
+    },
+    Thunderstorm:{
+        iconName:"weather-lightning",
+        gradient: ["#485563","#29323c"]
+    },
+    Drizzle:{
+        iconName:"weather-rainy",
+        gradient: ["#5f2c82","#49a09d"]
+    },
+    Rain:{
+        iconName:"weather-pouring",
+        gradient: ["#005C97","##363795"]
+    },
+    Snow:{
+        iconName:"weather-snowy",
+        gradient: ["#70e1f5","#ffd194"]
+    },
+    Clear:{
+        iconName:"weather-sunny",
+        gradient: ["#FFB75E","#ED8F03"]
+    },
+    Clouds:{
+        iconName:"weather-cloudy",
+        gradient: ["#304352","#d7d2cc"]
+    },
+    Mist:{
+        iconName:"weather-fog",
+        gradient: ["#b993d6","#8ca6db"]
+    },
+    Dust:{
+        iconName:"weather-fog",
+        gradient: ["#b993d6","#8ca6db"]
+    }
+}
+
+export default function Weather({temp,condition}){
     return (
-    <View style ={styles.container}>
+    <LinearGradient
+          colors={weatherOptions[condition].gradient}
+          style={styles.container}>
+        <StatusBar barStyle ="light-content"/>
         <View style = {styles.Uphalf}>
-        <MaterialCommunityIcons size = {96} name = "weather-lightning-rainy"/>
+        <MaterialCommunityIcons size = {96} name = {weatherOptions[condition].iconName} color = "white"/>
         <Text style = {styles.temp}>{temp}•</Text>
         </View>
         <View style = {styles.Downhalf}>
         </View>
-    </View>
+        </LinearGradient>
     );
 }
 
@@ -35,6 +79,7 @@ const styles = StyleSheet.create({
         flex:1
     },
     temp:{
-        fontSize:42
+        fontSize:42,
+        color:"white"
     }
 });
